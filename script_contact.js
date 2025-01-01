436bb4b653c0147596c985e1ed80ee10cfdb9e8e
Romain Wirth <romain.wirth@gmail.com>
2025-01-01 13:09:11 +0100

    Ajout remerciement_contact.html (vierge) + modification body + ajout footer et nav bar sur toutes les pages

--- script_contact.js
+++ script_contact.js
@@ -2,6 +2,10 @@ let mail = document.querySelector("#mail");
 let telephone = document.querySelector("#telephone");
 let formulaire = document.querySelector("#contact_formulaire");
 let erreur_formulaire = document.querySelector("#erreur_formulaire");
+let choix_mail = document.querySelector("#choix_mail");
+let choix_telephone = document.querySelector("#choix_telephone")
+let erreur_choix_formulaire = document.querySelector("#erreur_choix_formulaire");
+
 
 formulaire.addEventListener("submit", (e) => {
     e.preventDefault();  // Empêche la soumission classique du formulaire
@@ -9,18 +13,29 @@ formulaire.addEventListener("submit", (e) => {
     // Réinitialise les messages d'erreur
     erreur_formulaire.innerHTML = "";  
 
-    let mailValue = mail.value.trim();
-    let telephoneValue = telephone.value.trim();
+    let mail_value = mail.value.trim();
+    let telephone_value = telephone.value.trim();
+   
 
     // Vérifie si les champs mail et téléphone sont vides
-    if (mailValue === "" && telephoneValue === "") {
-        let error = document.createElement("div");
-        error.textContent = "Veuillez sélectionner un moyen de contact.";
-        error.style.color = "red";
+    if (mail_value === "" && telephone_value === "") {
+        let erreur = document.createElement("div");
+        erreur.textContent = "Veuillez renseigner votre adresse mail ou numéro de téléphone";
+        erreur.style.color = "red";
         erreur_formulaire.appendChild(error);
-    } else {
+     }
+
+    else if (!choix_mail.checked && !choix_telephone.checked) {
+        let erreur_choix = document.createElement("div");
+        erreur_choix.textContent = "Veuillez choisir un moyen de contact:";
+        erreur_choix.style.color = "red"
+        erreur_choix_formulaire.appendChild(erreur_choix);
+    }
+
+    else {
         // Si tout va bien, montre un message de confirmation
         alert("Merci pour votre envoi, nous reviendrons vers vous le plus vite possible.");
+        formulaire.textContent = "Merci d'avoir pris contact avec nous, nous reviendrons vers vous dans les plus brefs délais pour vous répondre. "
     }
 });
 
