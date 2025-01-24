d2b19e96cde721a444b78843dfce76b7775eaa06
Romain Wirth <romain.wirth@gmail.com>
2025-01-24 23:38:12 +0100

    ajout scrolel left and right

--- script_main.js
+++ script_main.js
@@ -1,6 +1,79 @@
+const positions = {};
+const timestamp = {}
+
+function textScrollRight(container, texte) {
+  const id = container.id
+  const largeur_texte = texte.offsetWidth;
+  const largeur_container = container.offsetWidth
+
+
+  if(!positions[id]) {
+    positions[id] = -largeur_texte;
+    timestamp[id] = 0;
+  }
+
+  function step(currentTimestamp) {
+  
+  if (!timestamp[id]) timestamp[id] = currentTimestamp;
+  const elapsed = currentTimestamp - timestamp[id];
+
+  if (elapsed >= 16) {
+    positions[id] += 1.5
+
+    if (positions[id] >= largeur_container) {
+    positions[id] = -largeur_texte;
+  }
+
+  texte.style.transform = `translateX(${positions[id]}px)`;
+  timestamp[id] = currentTimestamp;
+}
+  requestAnimationFrame(step);
+ }
+
+ requestAnimationFrame(step);
+
+}
+
+
+
+ function textScrollLeft(container, texte) {
+   const id = container.id;
+   const largeur_texte = texte.offsetWidth;
+   const largeur_container = container.offsetWidth;
+ 
+   if (!positions[id]) {
+     positions[id] = largeur_container;
+     timestamp[id] = 0
+   }
+ 
+   function step(currentTimestamp) {
+     if (!timestamp[id]) timestamp[id] = currentTimestamp;
+     const elapsed = currentTimestamp - timestamp[id];
+ 
+     if (elapsed >= 16) { // Environ 60 FPS
+       positions[id] -= 1.5;
+ 
+       if (positions[id] <= -largeur_texte) {
+         positions[id] = largeur_container;
+       }
+ 
+       texte.style.transform = `translateX(${positions[id]}px)`;
+       timestamp[id] = currentTimestamp;
+     }
+ 
+     requestAnimationFrame(step);
+   }
+ 
+   requestAnimationFrame(step);
+ }
+
+
+
+
+
 function pageIndex() {
 
-  //_blank permet d'ouvrir un nouvel onglet plutôt qu'une nouvelle fenêtre//
+  //_blank permet d'ouvrir un nouvel onglet plutÃ´t qu'une nouvelle fenÃªtre//
   let facebook = document.getElementById("facebook");
   facebook.addEventListener("click", () => {
       window.open("https://www.facebook.com/share/18P1oWswhE/", "_blank");
@@ -8,10 +81,10 @@ function pageIndex() {
   ;
      
   
-      // Liste des images à mettre à jour
+      // Liste des images Ã  mettre Ã  jour
   const images = document.querySelectorAll("img");
   
-  // Appliquer un timestamp unique à chaque image
+  // Appliquer un timestamp unique Ã  chaque image
   images.forEach(image => {
     const src = image.getAttribute("src");
     image.setAttribute("src", src + "?v=" + new Date().getTime());
@@ -49,6 +122,9 @@ function pageIndex() {
   setInterval(afficherMeteo, 120000 );
   
   async function afficherMeteo(){
+
+    document.querySelector('.container_meteo').style.display = "none"
+
     const requete = await fetch(url, {
       method: "GET"
     })
@@ -72,53 +148,69 @@ function pageIndex() {
           container_icone_meteo.append(icone_meteo)
   
           let temperature = reponse.main.temp;
-          document.querySelector("#temperature").textContent =`${Math.round(temperature)}°C`;
+          document.querySelector("#temperature").textContent =`${Math.round(temperature)} °C`;
+          document.querySelector(".container_meteo").style.display = "flex"
         
         }
         
       else {
         let container_meteo = document.querySelector("#container_meteo")
         container_meteo.remove()
-        throw new Error("Impossible de charger la météo.")
+        throw new Error("Impossible de charger la mété©o.")
       }
       }
   
 }
 
 
+function pagePresentation(){
+
+const container_club = document.querySelector("#container_club");
+const texte_club = document.querySelector("#texte_club");
+
+textScrollLeft(container_club, texte_club)
+textScrollRight(container_animateurs, texte_animateurs)
+
+
+
+}
 
 
 function pageSeances() {
 
-  const container_horaires = document.querySelector("#container_horaires")
-  const texte_horaires = document.querySelector("#texte_horaires")
-  let position = container_horaires.offsetWidth;
-  
-  
-  function textScrollLeft(container, texte) {
-    
-    const largeur_texte = texte.offsetWidth;
-      
-      position -= 1.5
-  
-      if (position < -largeur_texte) {
-          position = container.offsetWidth
-      }
-  
-      texte.style.transform = `translateX(${position}px)`;
-  
-      requestAnimationFrame(() => textScrollLeft(container, texte))
-  
-  }
-  
+  const container_horaires = document.querySelector("#container_horaires");
+  const texte_horaires = document.querySelector("#texte_horaires");
+  const container_sites= document.querySelector("#container_sites");
+  const texte_sites = document.querySelector("#texte_sites");
+
   textScrollLeft(container_horaires, texte_horaires)
+  textScrollRight(container_sites, texte_sites)
+}
+
+function pageMarcheAquatique() {
+  const container_marche = document.querySelector("#container_marche");
+  const texte_marche = document.querySelector("#texte_marche");
+  const container_bienfaits = document.querySelector("#container_bienfaits");
+  const texte_bienfaits = document.querySelector("#texte_bienfaits")
+
+  textScrollLeft(container_marche, texte_marche)
+  textScrollRight(container_bienfaits, texte_bienfaits)
 }
 
 // Appel des fonctions en fonction de la page
 if (window.location.href.includes('index')) {
   pageIndex();
+}
 
-} else if (window.location.href.includes('seances')) {
+else if (window.location.href.includes("presentation")) {
+  pagePresentation();
+}
+
+ else if (window.location.href.includes('seances')) {
   
   pageSeances();
+}
+
+else if (window.location.href.includes("marche_aquatique")) {
+  pageMarcheAquatique()
 }
